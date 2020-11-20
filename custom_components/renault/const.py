2c37c53d7d112f9a7ba362bbe398b51837db45cf
epenet <6771947+epenet@users.noreply.github.com>
2020-11-20 13:58:50 +0100

    Use AVAILABLE_LOCALES from new renault_api package (#74)
    * Use AVAILABLE_LOCALES from new renault_api package
    
    * Undo changes to requirements.txt

--- custom_components/renault/const.py
+++ custom_components/renault/const.py
@@ -9,38 +9,6 @@ CONF_KAMEREON_ACCOUNT_ID = "kamereon_account_id"
 MODEL_SUPPORTS_LOCATION = ["X102VE"]
 MODEL_USES_KWH = ["X102VE"]
 
-AVAILABLE_LOCALES = [
-    "bg_BG",
-    "cs_CZ",
-    "da_DK",
-    "de_DE",
-    "de_AT",
-    "de_CH",
-    "en_GB",
-    "en_IE",
-    "es_ES",
-    "es_MX",
-    "fi_FI",
-    "fr_FR",
-    "fr_BE",
-    "fr_CH",
-    "fr_LU",
-    "hr_HR",
-    "hu_HU",
-    "it_IT",
-    "it_CH",
-    "nl_NL",
-    "nl_BE",
-    "no_NO",
-    "pl_PL",
-    "pt_PT",
-    "ro_RO",
-    "ru_RU",
-    "sk_SK",
-    "sl_SI",
-    "sv_SE",
-]
-
 REGEX_VIN = "(?i)^VF1[\\w]{14}$"
 
 SUPPORTED_PLATFORMS = [
