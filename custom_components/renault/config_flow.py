848815030bf630297ce45c96e36bdf87d6901f4f
epenet <6771947+epenet@users.noreply.github.com>
2020-11-30 22:10:40 +0100

    Merge first and second step in config_flow Add unique_id

--- custom_components/renault/config_flow.py
+++ custom_components/renault/config_flow.py
@@ -32,9 +32,14 @@ class RenaultFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         """
         if user_input:
             locale = user_input[CONF_LOCALE]
-            user_input.update(AVAILABLE_LOCALES[locale])
             self.renault_config.update(user_input)
-            return await self.async_step_credentials()
+            self.renault_config.update(AVAILABLE_LOCALES[locale])
+            self.renault_hub = RenaultHub(self.hass, locale)
+            if not await self.renault_hub.attempt_login(
+                user_input[CONF_USERNAME], user_input[CONF_PASSWORD]
+            ):
+                return self._show_user_form({"base": "invalid_credentials"})
+            return await self.async_step_kamereon()
         return self._show_user_form()
 
     def _show_user_form(self, errors=None):
@@ -44,29 +49,6 @@ class RenaultFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
             data_schema=vol.Schema(
                 {
                     vol.Required(CONF_LOCALE): vol.In(AVAILABLE_LOCALES.keys()),
-                }
-            ),
-            errors=errors if errors else {},
-        )
-
-    async def async_step_credentials(self, user_input=None):
-        """Request credentials."""
-        if user_input:
-            self.renault_config.update(user_input)
-            self.renault_hub = RenaultHub(self.hass, self.renault_config[CONF_LOCALE])
-            if not await self.renault_hub.attempt_login(
-                user_input[CONF_USERNAME], user_input[CONF_PASSWORD]
-            ):
-                return self._show_credentials_form({"base": "invalid_credentials"})
-            return await self.async_step_kamereon()
-        return self._show_credentials_form()
-
-    def _show_credentials_form(self, errors=None):
-        """Show the credentials form."""
-        return self.async_show_form(
-            step_id="credentials",
-            data_schema=vol.Schema(
-                {
                     vol.Required(CONF_USERNAME): str,
                     vol.Required(CONF_PASSWORD): str,
                 }
@@ -77,6 +59,9 @@ class RenaultFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_kamereon(self, user_input=None):
         """Select Kamereon account."""
         if user_input:
+            await self.async_set_unique_id(user_input[CONF_KAMEREON_ACCOUNT_ID])
+            self._abort_if_unique_id_configured()
+
             self.renault_config.update(user_input)
             return self.async_create_entry(
                 title=user_input[CONF_KAMEREON_ACCOUNT_ID], data=self.renault_config
@@ -86,6 +71,9 @@ class RenaultFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         if len(accounts) == 0:
             return self.async_abort(reason="kamereon_no_account")
         if len(accounts) == 1:
+            await self.async_set_unique_id(accounts[0])
+            self._abort_if_unique_id_configured()
+
             self.renault_config[CONF_KAMEREON_ACCOUNT_ID] = accounts[0]
             return self.async_create_entry(
                 title=self.renault_config[CONF_KAMEREON_ACCOUNT_ID],
