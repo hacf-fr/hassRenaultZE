e93a9e2ed8434b4bc4981d2f9a8317c0c6c303fe
epenet <6771947+epenet@users.noreply.github.com>
2021-01-04 22:51:42 +0100

    Add option specify the time between two API calls (#100)
    * Add config_flow option for scan interval
    
    * Update initialisation routine
    
    * Cleanup initialisation routine
    
    * Cleanup initialisation routine
    
    * Move scan interval to vehicle init
    
    * Update description text
    
    * Update description text

--- custom_components/renault/config_flow.py
+++ custom_components/renault/config_flow.py
@@ -5,12 +5,16 @@ from renault_api.const import AVAILABLE_LOCALES
 import voluptuous as vol
 
 from homeassistant import config_entries
-from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
+from homeassistant.const import CONF_PASSWORD, CONF_SCAN_INTERVAL, CONF_USERNAME
+from homeassistant.core import callback
+from homeassistant.helpers import config_validation as cv
 
 from .const import (  # pylint: disable=unused-import
     CONF_KAMEREON_ACCOUNT_ID,
     CONF_LOCALE,
+    DEFAULT_SCAN_INTERVAL,
     DOMAIN,
+    MIN_SCAN_INTERVAL,
 )
 from .renault_hub import RenaultHub
 
@@ -89,5 +93,37 @@ class RenaultFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
             step_id="kamereon",
             data_schema=vol.Schema(
                 {vol.Required(CONF_KAMEREON_ACCOUNT_ID): vol.In(accounts)}
+            ),
+        )
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry: config_entries.ConfigEntry):
+        return OptionsFlowHandler(config_entry)
+
+
+class OptionsFlowHandler(config_entries.OptionsFlow):
+    """Handle an option flow for Renault."""
+
+    def __init__(self, config_entry: config_entries.ConfigEntry):
+        """Initialize options flow."""
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        """Manage the options."""
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema(
+                {
+                    vol.Optional(
+                        CONF_SCAN_INTERVAL,
+                        default=self.config_entry.options.get(
+                            CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL
+                        ),
+                    ): vol.All(cv.positive_int, vol.Clamp(min=MIN_SCAN_INTERVAL)),
+                }
             ),
         )
--- custom_components/renault/renault_vehicle.py
+++ custom_components/renault/renault_vehicle.py
@@ -11,8 +11,6 @@ from homeassistant.helpers.typing import HomeAssistantType
 from .const import DOMAIN
 from .renault_coordinator import RenaultDataUpdateCoordinator
 
-DEFAULT_SCAN_INTERVAL = timedelta(minutes=5)
-
 LOGGER = logging.getLogger(__name__)
 
 
@@ -24,6 +22,7 @@ class RenaultVehicleProxy:
         hass: HomeAssistantType,
         vehicle: RenaultVehicle,
         details: models.KamereonVehiclesDetails,
+        scan_interval: timedelta,
     ) -> None:
         """Initialise vehicle proxy."""
         self.hass = hass
@@ -38,6 +37,7 @@ class RenaultVehicleProxy:
         }
         self.coordinators: Dict[str, RenaultDataUpdateCoordinator] = {}
         self.hvac_target_temperature = 21
+        self._scan_interval = scan_interval
 
     @property
     def details(self) -> models.KamereonVehiclesDetails:
@@ -58,7 +58,7 @@ class RenaultVehicleProxy:
             name=f"{self.details.vin} cockpit",
             update_method=self.get_cockpit,
             # Polling interval. Will only be polled if there are subscribers.
-            update_interval=DEFAULT_SCAN_INTERVAL,
+            update_interval=self._scan_interval,
         )
         self.coordinators["hvac_status"] = RenaultDataUpdateCoordinator(
             self.hass,
@@ -67,7 +67,7 @@ class RenaultVehicleProxy:
             name=f"{self.details.vin} hvac_status",
             update_method=self.get_hvac_status,
             # Polling interval. Will only be polled if there are subscribers.
-            update_interval=DEFAULT_SCAN_INTERVAL,
+            update_interval=self._scan_interval,
         )
         if self.details.uses_electricity():
             self.coordinators["battery"] = RenaultDataUpdateCoordinator(
@@ -77,7 +77,7 @@ class RenaultVehicleProxy:
                 name=f"{self.details.vin} battery",
                 update_method=self.get_battery_status,
                 # Polling interval. Will only be polled if there are subscribers.
-                update_interval=DEFAULT_SCAN_INTERVAL,
+                update_interval=self._scan_interval,
             )
             self.coordinators["charge_mode"] = RenaultDataUpdateCoordinator(
                 self.hass,
@@ -86,7 +86,7 @@ class RenaultVehicleProxy:
                 name=f"{self.details.vin} charge_mode",
                 update_method=self.get_charge_mode,
                 # Polling interval. Will only be polled if there are subscribers.
-                update_interval=DEFAULT_SCAN_INTERVAL,
+                update_interval=self._scan_interval,
             )
         self.coordinators["location"] = RenaultDataUpdateCoordinator(
             self.hass,
@@ -95,7 +95,7 @@ class RenaultVehicleProxy:
             name=f"{self.details.vin} location",
             update_method=self.get_location,
             # Polling interval. Will only be polled if there are subscribers.
-            update_interval=DEFAULT_SCAN_INTERVAL,
+            update_interval=self._scan_interval,
         )
         for key in list(self.coordinators.keys()):
             await self.coordinators[key].async_refresh()
